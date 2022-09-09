0d3e142d891223c6961b78a626e8b7fce72a50af
Jayromberg <jayro.dev@gmail.com>
2022-09-09 18:56:07 -0300

    test: Adicionado os testes do tipo de array e o que lista todos os produtos

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,8 +1,16 @@
-const getAllProducts = () => [];
+const connection = require('./db/connection');
 
-const getProductsById = () => [];
+const findAllProducts = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products;',
+  );
+
+  return result;
+};
+
+const findAllProductsById = () => [];
 
 module.exports = {
-  getAllProducts,
-  getProductsById,
+  findAllProducts,
+  findAllProductsById,
 };
--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -3,13 +3,23 @@ const sinon = require('sinon');
 
 const connection = require('../../../src/models/db/connection');
 const { productsModel } = require('../../../src/models');
+const productsMock = require('./mocks/productsModelsMock');
 
 describe('Products Model', function () {
-  afterEach(async function () {
-    sinon.restore();
-  })
-  it('Lista todos os produtos', function () {
+  beforeEach(function () {
+    sinon.stub(connection, 'execute').resolves(productsMock.productsMockFromDB);
+  });
+  afterEach(function () {
+    sinon.restore;
+  });
+  it('com o tipo array', async function () {
+    const response = await productsModel.findAllProducts();
+    expect(response).to.be.a('array');
+  });
+  it('Lista todos os produtos', async function () {
+    const response = await productsModel.findAllProducts();
 
+    expect(response).to.deep.equal(productsMock.productsMockFromDB);
   });
 
   it('Lista o produto pelo id', function () {
