1ff36196391aa77acb6a180ea023206eb36a1340
Jayromberg <jayro.dev@gmail.com>
2022-09-10 09:53:29 -0300

    test: Adiciona o teste que busca todos os produtos em estoque

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -1,2 +1,27 @@
-const { expect } = require('chai');
+const chai = require('chai');
 const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+
+const productsControllers = require('../../../src/controllers/productsController');
+const { productsServices } = require('../../../src/services');
+const { servicesProductResponse, productNotFound } = require('./mocks/productsControllersMocks');
+
+describe('Products Controllers', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('Busca todos os produtos em estoque', async function () {
+    const res = {};
+    const req = {};
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon.stub(productsServices, 'getAllProducts').resolves(servicesProductResponse);
+    await productsControllers.getAllProducts(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(servicesProductResponse.message);
+  })
+});
--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -23,7 +23,7 @@ describe('Products Services', function () {
   });
 
   it('Produto não encontrado', async function () {
-    sinon.stub(productsModel, 'findProductsById').resolves(undefined);
+    sinon.stub(productsModel, 'findProductsById').resolves([]);
     const product = await productsServices.getProductsByID(5);
     expect(product.message).to.be.equal('Product not found');
     expect(product.type).to.be.equal('NOT_FOUND');
