fa7eaa2b6f308ee28bafd4f867e2892073517ef9
Jayromberg <jayro.dev@gmail.com>
2022-09-11 19:51:50 -0300

    test: Adiciona o teste de sales services

---
+++ tests/unit/services/salesService.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { salesModel } = require('../../../src/models');
+const { salesService } = require('../../../src/services');
+const { objectWithTheProductsSold, objectWithSalesReturned } = require('./mocks/salesServiceMcks');
+
+describe('Sales Service', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('Insere as vendas no banco de dados', async function () {
+    sinon.stub(salesModel, 'insertSalesModel').resolves({ insertId: 3 });
+    const sales = await salesService.insertSalesService(objectWithTheProductsSold);
+    expect(sales).to.deep.equal(objectWithSalesReturned);
+  });
+});
