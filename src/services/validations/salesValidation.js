94418476335827b40ed9e19ccef908b83f428ccf
Jayromberg <jayro.dev@gmail.com>
2022-09-12 11:55:39 -0300

    feat: Adicionado as validações em insertSalesServices

---
+++ src/services/validations/salesValidation.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+const { productsModel } = require('../../models');
+
+const salesValidation = (product) => {
+  const schema = Joi.object({
+    productId: Joi.number().required()
+      .messages({
+        'any.required': 'PRODUCT_ID_IS_REQUIRED',
+      }),
+    
+    quantity: Joi.number().integer().positive().required()
+      .messages({
+        'number.positive': 'INVALID_QUANTITY',
+        'any.required': 'QUANTITY_IS_REQUIRED',
+      }),
+  }).length(2);
+
+  return schema.validate(product);
+};
+
+const validateInputProductId = async (sales) => {
+  let validate = true;
+
+  const generatePromises = sales
+    .map((sale) => productsModel.findProductsByIdModel(sale.productId));
+
+  const products = await Promise.all(generatePromises);
+
+  products.forEach(([product]) => {
+    if (!product) {
+      validate = false;
+    }
+  });
+
+  return validate;
+};
+
+module.exports = {
+  salesValidation,
+  validateInputProductId,
+};
--- src/utils/mapError.js
+++ src/utils/mapError.js
@@ -11,6 +11,18 @@ const errorMap = {
     code: 422,
     message: '"name" length must be at least 5 characters long',
   },
+  PRODUCT_ID_IS_REQUIRED: {
+    code: 400,
+    message: '"productId" is required',
+  },
+  QUANTITY_IS_REQUIRED: {
+    code: 400,
+    message: '"quantity" is required',
+  },
+  INVALID_QUANTITY: {
+    code: 422,
+    message: '"quantity" must be greater than or equal to 1',
+  },
 };
 
 const mapError = (type) => errorMap[type] || { code: 500, message: 'Internal error' };
